73938476b5619c98c667dce76256564c0678f267
Jovan Josafat <jovan.josafat@gmail.com>
2023-12-18 21:24:10 +0700

    update

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ import sys
 from gunicorn.app.wsgiapp import run
 
 if __name__ == '__main__':
-    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.argv = "gunicorn --bind 0.0.0.0:3000 app:app".split()
     sys.exit(run())
