d9429991991d16e86b7b1e19876d0513b2bbf8c8
Jovan Josafat <jovan.josafat@gmail.com>
2023-12-19 20:11:30 +0700

    update

--- database.py
+++ database.py
@@ -3,21 +3,23 @@ from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# db_host = os.environ.get("INSTANCE_HOST")
+db_host = os.environ.get("INSTANCE_HOST")
 db_user = os.environ.get("DB_USER")
 db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
-# db_port = os.environ.get("DB_PORT")
+db_port = os.environ.get("DB_PORT")
 unix_socket_path = os.environ[
     "INSTANCE_UNIX_SOCKET"
 ]
 
 url_object = URL.create(
     drivername="mysql+pymysql",
+    host=db_host,
     username=db_user,
     password=db_pass,
     database=db_name,
-    query={"unix_socket": unix_socket_path}
+    port=db_port
+    # query={"unix_socket": unix_socket_path}
 )
 engine = create_engine(url_object)
 db_session = scoped_session(sessionmaker(autocommit=False,
